1e3748e239a544e718a46c8c61664e3ecec8e5f7
DevanshuDesai15 <devanshudesai_1509@yahoo.com>
2024-06-13 18:54:49 -0700

    article list component made reusable.

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,7 +1,11 @@
+import ArticlesList from "../sub-components/ArticlesList.jsx";
+import articles from "../sub-components/ArticleContent.jsx";
+
 const ArticleList = () => {
     return (
-        <div>
-        <h1>Article List</h1>
+        <div className="flex flex-col justify-start">
+            <h1 className="text-4xl font-bold mb-4 w-full text-center">Articles</h1>
+            <ArticlesList articles={articles} />
         </div>
     );
 }
---
+++ src/sub-components/ArticlesList.jsx
@@ -0,0 +1,21 @@
+import {Link} from "react-router-dom";
+
+// eslint-disable-next-line react/prop-types
+const ArticlesList = ({ articles }) => {
+    return (
+        <>
+            {/* eslint-disable-next-line react/prop-types */}
+            {articles.map((article, key) => (
+                <Link to={`/articles/${article.name}`} key={key} className="w-full mb-4">
+                    <div className="m-2 p-4 border-b-4 border-gray-300 text-left">
+                        <h3 className="text-xl font-bold mb-2">{article.title}</h3>
+                        <p className="text-base">{article.content[0].substring(0, 150)}...</p>
+                    </div>
+                </Link>
+            ))}
+        </>
+    )
+
+}
+
+export default ArticlesList;
