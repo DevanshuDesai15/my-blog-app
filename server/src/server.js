f7e9c0935fb92a728a6a904f7214e475ae9c4437
DevanshuDesai15 <devanshudesai_1509@yahoo.com>
2024-06-27 13:16:26 -0700

    added cors for accesing the backed data from frontend

--- server/src/server.js
+++ server/src/server.js
@@ -1,18 +1,23 @@
-import express from 'express';
-import articles from '../routes/articles.js';
-import mongoose from 'mongoose';
+import express from "express";
+import articles from "../routes/articles.js";
+import mongoose from "mongoose";
+import cors from "cors";
 
-mongoose.connect('mongodb://127.0.0.1/blogger', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb://127.0.0.1/blogger", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.log('Connected to database');
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Connected to database");
 });
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-app.use('/articles', articles);
+app.use("/articles", articles);
 
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+  console.log("Server is running on http://localhost:3000");
 });
