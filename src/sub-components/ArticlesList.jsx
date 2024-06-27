1ebb8937fd0d25ee47abcebd52188fe7ef60acd8
DevanshuDesai15 <devanshudesai_1509@yahoo.com>
2024-06-27 13:16:55 -0700

    update: article list displayed from backend

--- src/sub-components/ArticlesList.jsx
+++ src/sub-components/ArticlesList.jsx
@@ -1,4 +1,4 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 // eslint-disable-next-line react/prop-types
 const ArticlesList = ({ articles }) => {
@@ -6,10 +6,10 @@ const ArticlesList = ({ articles }) => {
         <>
             {/* eslint-disable-next-line react/prop-types */}
             {articles.map((article, key) => (
-                <Link to={`/articles/${article.name}`} key={key} className="w-full mb-4  hover:text-black">
+                <Link to={`/articles/${article._id}`} key={key} className="w-full mb-4  hover:text-black">
                     <div className="m-2 p-4 border-b-4 border-gray-300 hover:bg-blue-300 hover:border-black text-left">
                         <h3 className="text-xl font-bold mb-2">{article.title}</h3>
-                        <p className="text-base">{article.content[0].substring(0, 150)}...</p>
+                        <p className="text-base">{article.content.substring(0, 150)}...</p>
                     </div>
                 </Link>
             ))}
